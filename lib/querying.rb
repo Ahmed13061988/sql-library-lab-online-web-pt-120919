e6942458a619005700e4f952a9ba3ce64caee2c6
Ahmed Hussein <wolktheline@yahoo.com>
2020-02-23 19:43:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,7 +44,7 @@ def select_series_title_with_most_human_characters	def select_series_title_with_
 end	
 
 
-def select_character_names_and_number_of_books_they_are_in	def select_character_names_and_number_of_books_they_are_in
+def select_character_names_and_number_of_books_they_are_in	
   "SELECT characters.name, COUNT(name)
   FROM character_books
   INNER JOIN characters
