042a72b0faf60033bb78973bc42f8efbd3d7c5ae
Ahmed Hussein <wolktheline@yahoo.com>
2020-02-23 19:36:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,9 +28,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  
+   "SELECT species, COUNT(species) AS num_species FROM characters
+    GROUP BY species
+    ORDER BY num_species DESC
+    LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  
 end
